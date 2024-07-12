d6935a99fbcb3194438d09080f38faea02765cbb
Vikas Rajpoot <120569817+vikasrajpoot30@users.noreply.github.com>
2024-07-12 17:17:26 +0000

    conversion of data types

---
+++ 01_basics/03_data_type_conversion.js
@@ -0,0 +1,13 @@
+// let score="33";
+let score="33abc";
+
+
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// ""  => false in boolean 
+// "vikas" => true in boolean
+
+
