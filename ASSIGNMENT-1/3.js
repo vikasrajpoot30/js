2f66a0aa1bc4606708186c9a47e32197e4be9df7
Vikas Rajpoot <120569817+vikasrajpoot30@users.noreply.github.com>
2024-09-26 06:49:37 +0000

    Q3

---
+++ ASSIGNMENT-1/3.js
@@ -0,0 +1,12 @@
+const reverseString=(str)=>{
+    let result=''
+    for(let i=str.length-1;i>=0;i--){
+        result+=str[i];
+    }
+    return result;
+}
+
+
+
+
+console.log(reverseString('fileShere')) // elif
