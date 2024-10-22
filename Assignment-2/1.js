56ee782fdddf585f3e6926473af6a75840fa7585
Vikas Rajpoot <120569817+vikasrajpoot30@users.noreply.github.com>
2024-10-22 05:46:55 +0000

    Q1

---
+++ Assignment-2/1.js
@@ -0,0 +1,22 @@
+// Assignment test datw 22 oct 2024 time- 11:13AM
+
+// signed - vikasrajpoot
+
+const sounds = [
+    {
+    name: 'rain',
+    sound: 'tap tap tap',
+    },
+    {
+    name: 'fire',
+    sound: 'blaze',
+    },
+    {
+    name: 'water',
+    sound: 'slosh slosh',
+    },
+   ]
+
+   const capitalizeLetters=(sounds)=>sounds.map(sound=>sound.name.toUpperCase())
+   console.log(capitalizeLetters(sounds)) // Output: ["RAIN", "FIRE", "WATER"]
+   
